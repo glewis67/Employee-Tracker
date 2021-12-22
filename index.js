9f6acc9306c8a4d8a7540abc54d4f5157afd459b
glewis67 <84549006+glewis67@users.noreply.github.com>
2021-12-22 17:41:58 -0600

    add files

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 const db=require("./db/connection")
+const inquirer=require("inquirer")
+
+async function init (){
+   const data=await inquirer.prompt(
+        {
+            type:"list",
+            name:"choice",
+            message:"What would you like to do?",
+            choices:["view all departments","view all roles","view all employees","add a department","add a role","add an employee","update an employee role"]
+        }
+    )
+    // .then((data) =>{
+    //     console.log(data)
+        switch(data.choice){
+            case "view all departments":
+                viewAllDepartments()
+                break
+            case "view all roles":
+                viewAllRoles()
+                break
+            case "view all employees":
+                viewAllEmployees()
+                break
+            case "add a department":
+                addADepartment()
+                break
+            case"add a role":
+                addARole()
+                break
+            case"add an employee":
+            addAnEmployee()
+                break
+            case"update an employee role":
+            updateAnEmployeeRole()
+            break
+            // default:
+            // break
+
+        }
+
+        
+        
+    // })
+}
+function viewAllDepartments(){
+    const sql="SELECT * FROM department"
+    db.query(sql, (err,result)=>{
+        if(err)throw err
+        console.table(result)
+        init()
+    }) 
+}
+
+async function start(){
+    await init()
+}
+start()
