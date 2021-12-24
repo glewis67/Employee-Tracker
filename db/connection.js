06a0f6aa79d4a92e11f9d92e606c4268f7bd18a9
glewis67 <84549006+glewis67@users.noreply.github.com>
2021-12-23 18:46:30 -0600

    add file

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,10 @@
 const mysql =require("mysql2")
+require("dotenv").config()
 const connection= mysql.createConnection({
     host:"localhost",
     user:"root",
     password:process.env.DB_PW,
+    
     database:"employee_tracker"
 })
 connection.connect((err)=>{if (err)throw err})
--- index.js
+++ index.js
@@ -34,8 +34,8 @@ async function init (){
             case"update an employee role":
             updateAnEmployeeRole()
             break
-            // default:
-            // break
+            default:
+            break
 
         }
 
@@ -51,7 +51,202 @@ function viewAllDepartments(){
         init()
     }) 
 }
+function viewAllRoles(){
+    const sql="SELECT * FROM role"
+    db.query(sql, (err,result)=>{
+        if(err)throw err
+        console.table(result)
+        init()
+    })   
+}
+function viewAllEmployees(){
+    const sql=`SELECT employee.id,employee.first_name,employee.last_name,CONCAT(manager.first_name,' ',manager.last_name) AS manager,role.salary,role.title,department.name AS department
+ FROM  employee
+ LEFT JOIN role
+ ON employee.role_id=role.id
+ LEFT JOIN department
+ ON role.department_id=department.id
+ LEFT JOIN employee manager
+ ON employee.manager_id=manager.id`
+ db.query(sql, (err,result)=>{
+    if(err)throw err
+    console.table(result)
+    init()
+})   
+}
+async function addADepartment(){
+   const {name}=await inquirer.prompt({
+        name:"name",
+        message:"What is the name of the department?"
 
+    }) 
+    const sql="INSERT INTO department (name) VALUES (?)"
+    db.query(sql,name, (err,result)=>{
+        if(err)throw err
+        console.log(`${name} added`)
+        init()
+    })      
+}
+async function addARole(){
+    const sql="SELECT * FROM department"
+    db.query(sql, (err,result)=>{
+        if(err)throw err
+        
+        const departmentChoice=result.map(({id,name})=>({
+            name:name,
+            value:id
+        }))
+       inquirer.prompt([
+            {
+                name:"title",
+                message:"What is name of the role?"
+            },
+
+            {
+                name:"salary",
+                message:"What is the salary?"
+
+            },
+            {
+                type:"list",
+                name:"department_id",
+                message:"What is the department?",
+                choices:departmentChoice
+            }
+        ])
+        .then(data =>{
+            const params=[data.title,data.salary,data.department_id]
+            const sql="INSERT INTO role (title,salary,department_id) VALUES (?,?,?)"
+            db.query(sql,params, (err,result)=>{
+                if(err)throw err
+                console.log(`${data.title} added`)
+                init()
+            })      
+        })
+    })      
+}
+async function addAnEmployee(){
+const  roleArray=await findRole()
+const managerArray=await findManager()
+const roleChoice=roleArray.map(({id,title})=>({
+    name:title,
+    value:id
+}))
+const managerChoice=managerArray.map(({id,first_name,last_name})=>({
+    name:first_name+" "+last_name,
+    value:id
+}))
+
+inquirer.prompt([
+    {
+     name:"first_name",
+     message:"What is the employee first name"  
+    },
+    {
+        name:"last_name",
+        message:"What is the employee last name"  
+       },
+    {
+        type:"list",
+        name:"manager",
+        message:"Who is the manager",
+        choices:managerChoice
+    },
+    {
+        type:"list",
+        name:"role",
+        message:"What is the role?",
+        choices:roleChoice
+    }
+    
+])
+.then(data=>{
+    
+    const sql="INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUES (?,?,?,?)"
+    const params=[data.first_name,data.last_name,data.role,data.manager]
+    db.query(sql,params, (err,result)=>{
+        if(err)throw err
+        console.log(`${data.first_name} added`)
+        init()
+    })      
+})
+}
+function findRole(){
+    return new Promise(resolve =>{
+        const sql="SELECT * FROM role"
+        db.query(sql, (err,result)=>{
+            if(err)throw err
+           resolve(result)
+        })      
+    })
+}
+function findManager(){
+    return new Promise(resolve =>{
+    const sql="SELECT * FROM employee"
+    db.query(sql, (err,result)=>{
+        if(err)throw err
+       resolve(result)
+    }) 
+})     
+}
+// function findManager(){
+//     return new Promise(resolve =>{
+//  const sql="SELECT CONCAT(manager.first_name,' ',manager.last_name) AS manager FROM employee LEFT JOIN employee manager ON employee.manager_id=manager.id"       
+// let manager=[]
+// let map={}
+// db.query(sql, (err,result)=>{
+//     if(err)throw err
+//     for (let managerObj of result){
+//         if (managerObj.manager !==null){
+//             map[managerObj.manager=true]
+//         }
+//     }
+//     for(let key in map){
+//         manager.push(key)
+//     }
+//    resolve(manager)
+// })      
+// })
+// }
+async function updateAnEmployeeRole(){
+    const  roleArray=await findRole()
+    const employeeArray=await findManager()
+    const roleChoice=roleArray.map(({id,title})=>({
+        name:title,
+        value:id
+    }))
+    const employeeChoice=employeeArray.map(({id,first_name,last_name})=>({
+        name:first_name+" "+last_name,
+        value:id
+    }))  
+    inquirer.prompt([
+        {
+            type:"list", 
+            name:"employee",
+            message:"Which employee would like to update?",
+            choices:employeeChoice,
+        },
+        {
+            type:"list", 
+            name:"role",
+            message:"What is the employee new role?",
+            choices:roleChoice,
+        },
+    ])
+    .then(data =>{
+        // console.log(data.employee)
+        // let firstName=data.employee
+        // firstName=firstName.split(/(\s+)/)
+        // firstName=firstName[0]
+        const sql="UPDATE employee SET role_id = ? WHERE id = ?"
+        const params=[data.role,data.employee]
+        db.query(sql,params, (err,result)=>{
+            if(err)throw err
+            // console.log(`${data.firstName} added`)
+            init()
+        })      
+    })  
+}
 async function start(){
     await init()
 }
